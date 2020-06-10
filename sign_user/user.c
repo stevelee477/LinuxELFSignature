671869b64cb32207e0a7dd19cbf805a7c6716ebe
OrangeGzY <1193106810@qq.com>
2020-06-10 15:32:54 +0800

    User:Write to proc and verify

--- sign_user/user.c
+++ sign_user/user.c
@@ -10,34 +10,59 @@
 #include <semaphore.h>
 #include <signal.h>
 #include <sys/ioctl.h>
-
+#include <string.h>
 static sem_t event_sem;
 static volatile sig_atomic_t interested_event = 0;
+
+
+
+char verify(char *path){
+    char buf[256];
+    int result; 
+    char sCommand[256] = {'\0'};
+    strncpy(sCommand,"./verify ",sizeof("./verify "));
+    strcat(sCommand,path);
+    strcat(sCommand," > tmp");
+    int ret = system(sCommand);
+    FILE *fp;
+    fp = fopen("./tmp","r");
+    fgets(buf,10,fp);
+    fclose(fp);
+    system("rm tmp");
+    result = buf[8]-'0';
+    printf("result is :%d\n",result);
+    return buf[8];      // 返回char类型的结果、result是int类型的结果
+}
+
 
 void sig_handler_event1(int sig) {
     interested_event = 1;
     sem_post(&event_sem);
 }
 
-static void * event_handler_thread_func() {
+static void * event_handler_thread_func() {     //线程对应的函数
+    printf("%s\n","event_handler_thread_func()");
     printf("in %s line %d\n", __func__, __LINE__);
     while(1){
         sem_wait(&event_sem);
+
         if (interested_event){
             printf("%s,%d, received interested_event signal.\n",__func__, __LINE__);
 
             char buf[256];
             int fd = open("/proc/mydev", O_RDWR);
-            int len = read(fd, buf, 256);
+            int len = read(fd, buf, 256);       // ./a.out PWD=/home/paxos/LinuxELFSignature/sign_user
             
             printf("%s", buf);
 
             lseek(fd, 0 , SEEK_SET);
 
-            char str[5];
-            scanf("%s", str);
+            char str[5]={'\0'};
+            //scanf("%s", str);
+            // int result;
+            // result = verify(buf);
+            str[0] = verify(buf);
             write(fd, str, 2);
-
             interested_event = 0;
         }
     }
@@ -50,10 +75,10 @@ int main(int argc, char *argv[]) {
         printf("Thread create failed%s.\n", strerror(errno));
         exit(1);
     }
-    sem_init(&event_sem, 0, 0);
+    sem_init(&event_sem, 0, 0);             // 起一个线程然后初始化信号量
 
     // Signal handler init
-    struct sigaction usr_action;
+    struct sigaction usr_action;            //hanlder初始化
     sigset_t block_mask;
     sigfillset (&block_mask);
     usr_action.sa_handler = sig_handler_event1;
